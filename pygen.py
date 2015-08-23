40523997d5e866e622edc9b88843c469b71730cd
James Milne <s4b3r6@gmail.com>
2015-08-23 15:59:59 +1000

    Cleaning pygen.py up a bit

--- pygen.py
+++ pygen.py
@@ -1,24 +1,39 @@
 import random
 import json
 
-# Load in the map of Cameroon, released to Public Domain, as our base map.
-basemap = json.load(open('basemap.geojson', 'r'))
-# Replace our references to Cameroon with something more fitting.
-basemap['properties']['name'] = "5th World"
-basemap['properties']['code'] = "5WR"
-basemap['_id'] = "fifth_world"
-# Determine the number of Large Cities we'll have, there has to be at least one.
-# Randomly choose one of the Large Cities to be the capital.
-# Determine the number of Small Cities we'll have.
-# Determine the number of Villages we'll have.
-# Determine the number of Ruins we'll have.
-# Assign a location to the Capital City.
-# Assign a location to each of the Large Cities.
-# Draw roads from the Capital City to each of the Large Cities.
-# Assign a location to each of the Small Cities.
-# Draw roads from the closest Large City to the closest Small City.
-# Assign a location to each of the Villages.
-# Sometimes draw a road from a Village to the closest Small City.
-# Assign a location to each of the Ruins.
-# Sometimes draw a road from a Ruin to the closest Village.
-# Export the finished map.
+def random_coordinates():
+    raise NotImplementedError
+
+def export_geojson(dictIn):
+    raise NotImplementedError
+
+def map_setup():
+    # Load in the map of Cameroon, released to Public Domain, as our base map.
+    basemap = json.load(open('basemap.geojson', 'r'))
+    # Replace our references to Cameroon with something more fitting.
+    basemap['properties']['name'] = "5th World"
+    basemap['properties']['code'] = "5WR"
+    basemap['_id'] = "fifth_world"
+    return basemap
+
+def main():
+    basemap = map_setup()
+    print(basemap)
+    # Determine the number of Large Cities we'll have, there has to be at least one.
+    # Randomly choose one of the Large Cities to be the capital.
+    # Determine the number of Small Cities we'll have.
+    # Determine the number of Villages we'll have.
+    # Determine the number of Ruins we'll have.
+    # Assign a location to the Capital City.
+    # Assign a location to each of the Large Cities.
+    # Draw roads from the Capital City to each of the Large Cities.
+    # Assign a location to each of the Small Cities.
+    # Draw roads from the closest Large City to the closest Small City.
+    # Assign a location to each of the Villages.
+    # Sometimes draw a road from a Village to the closest Small City.
+    # Assign a location to each of the Ruins.
+    # Sometimes draw a road from a Ruin to the closest Village.
+    # Export the finished map.
+
+if __name__ == "__main__":
+    main()
